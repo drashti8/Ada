fbf5eebc46eb4865eea7eeda541efd28396b7c4d
drashti8 <90024083+drashti8@users.noreply.github.com>
2021-10-03 15:45:40 +0530

    Add files via upload

---
+++ knapsack.py
@@ -0,0 +1,37 @@
+def knapsack(W, wt, val, n):
+    if (n == 0 or W == 0):
+        return 0
+
+    if (wt[n - 1] > W):
+
+        return knapsack(W, wt, val, n - 1)
+
+    else:
+
+        return max(val[n - 1] + knapsack(W - wt[n - 1], wt, val, n - 1), knapsack(W, wt, val, n - 1))
+
+
+val = []
+n = int(input("Enter values: "))
+for i in range(0, n):
+    ele = int(input())
+
+    val.append(ele)
+
+print(val)
+wt = []
+n = int(input("Enter weight of value : "))
+for i in range(0, n):
+    ele = int(input())
+
+    wt.append(ele)
+
+print(wt)
+
+
+
+W = int(input())
+
+n = len(val)
+
+print(knapsack(W, wt, val, n))
